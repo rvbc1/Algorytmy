185af942f7826724358629dbccbc5c7ecca508fc
Mateusz Szczygielski <mat.szczygielski1@gmail.com>
2020-05-22 17:24:00 +0200

    init gmres

---
+++ GMRES.jl
@@ -0,0 +1,97 @@
+using LinearAlgebra
+
+function gmres(A, x, b, tolerance, max_iterations)
+    n = length(A)
+    m,n = size(A,1), size(A,2)
+    r = b - A*x
+
+    b_norm = norm(b)
+    error = norm(r) / b_norm
+
+    sn = zeros(max_iterations, 1)
+    cs = zeros(max_iterations, 1)
+    e1 = zeros(max_iterations, 1)
+    e1[1] = 1
+    e = [error]
+    r_norm = norm(r)
+    Q = zeros(n, max_iterations)
+    Q[:,1] = r / r_norm
+    beta = r_norm * e1
+
+    k = 1
+    H = zeros(2, 1)
+
+    for k = 1:m
+        tmp = arnoldi(A, Q, k)
+        H = zeros(k + 1, k)
+        H[1:k+1, k] = tmp.h
+        Q[:, k+1] = tmp.q
+
+        tmp2 = apply_givens_rotation(H[1:k+1,k], cs, sn, k)
+        H[1:k+1, k] = tmp2.h
+        cs[k] = tmp2.cs_k
+        sn[k] = tmp2.sn_k
+
+        beta[k + 1] = -sn[k] * beta[k]
+        beta[k] = cs[k] * beta[k]
+        error = abs(beta[k + 1]) / b_norm
+
+        e = [e; error]
+
+        if (error <= tolerance)
+            break
+        end
+    end
+    y = H[1:k, 1:k] \ beta[1:k]
+    x = x + Q[:, 1:k] * y
+
+    return x
+end
+
+function arnoldi(A, Q, k)
+    q = A*Q[:,k]
+    h = zeros(k + 1)
+    for i = 1:k
+        h[i] = q' * Q[:, i]
+        q = q - h[i] * Q[:, i]
+    end
+    h[k + 1] = norm(q)
+    q = q / h[k + 1]
+    return x = (h=h, q=q)
+end
+
+function apply_givens_rotation(h, cs, sn, k)
+    for i = 1:k-1
+        temp   =  cs[i] * h[i] + sn[i] * h[i + 1]
+        h[i+1] = -sn[i] * h[i] + cs[i] * h[i + 1]
+        h[i]   = temp
+    end
+  
+    tmp = givens_rotation(h[k], h[k + 1])
+    cs_k = tmp.cs;
+    sn_k = tmp.sn
+  
+    h[k] = cs_k * h[k] + sn_k * h[k + 1]
+    h[k + 1] = 0.0
+    return x = (h=h, cs_k=cs_k, sn_k=sn_k)
+end
+
+function givens_rotation(v1, v2)
+    if (v1 == 0)
+        cs = 0
+        sn = 1
+    else
+        t = sqrt(v1^2 + v2^2)
+        cs = abs(v1) / t
+        sn = cs * v2 / v1
+    end
+    return x = (cs=cs, sn=sn)
+end
+
+A = [1 2; 3 4]
+x = [1; 1]
+b = [-5646; 54355]
+x0 = gmres(A, x, b, 0.5, 1000)
+
+println(x0)
+println(A*x0)
